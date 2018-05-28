0286e94bdf5f1e43a52fefb81774561ca1d1fb51
MacBook <macbook@joshua.gateway>
2018-05-28 18:10:37 +1000

    test commit

---
+++ __main__.py
@@ -0,0 +1,16 @@
+"""
+	Entry file for game
+"""
+
+from core.Game import Game
+
+def main():
+
+	"""
+		Entry part of game
+	"""
+
+	game = Game()
+
+if __name__ == '__main__':
+	main()
---
+++ core/Game.py
@@ -0,0 +1,15 @@
+"""
+	Main game file
+"""
+
+class Game(object):
+
+	"""
+		Main game class
+	"""
+
+	def __init__(self):
+
+		super(Game, self).__init__()
+
+		print "Game initialised."
